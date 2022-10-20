63f0fb88e8e3cb4071ee5cb353a8948cfcc9ed67
HEE-WON, LEE <81808758+leeheewon-01@users.noreply.github.com>
2022-10-20 19:04:18 +0900

    Update finger.py

--- finger.py
+++ finger.py
@@ -1,35 +1,4 @@
-# 원본 코드 https://google.github.io/mediapipe/solutions/hands.html
-
-
-"""The 21 hand landmarks."""
-# 손가락 위치 정의 참고 https://google.github.io/mediapipe/images/mobile/hand_landmarks.png
-#
-# WRIST = 0
-# THUMB_CMC = 1
-# THUMB_MCP = 2
-# THUMB_IP = 3
-# THUMB_TIP = 4    엄지
-# INDEX_FINGER_MCP = 5
-# INDEX_FINGER_PIP = 6
-# INDEX_FINGER_DIP = 7
-# INDEX_FINGER_TIP = 8  검지
-# MIDDLE_FINGER_MCP = 9
-# MIDDLE_FINGER_PIP = 10
-# MIDDLE_FINGER_DIP = 11
-# MIDDLE_FINGER_TIP = 12  중지
-# RING_FINGER_MCP = 13
-# RING_FINGER_PIP = 14
-# RING_FINGER_DIP = 15
-# RING_FINGER_TIP = 16  약지
-# PINKY_MCP = 17
-# PINKY_PIP = 18
-# PINKY_DIP = 19
-# PINKY_TIP = 20  새끼
-
-
-# 필요한 라이브러리
-# pip install opencv-python mediapipe pillow numpy
-
+#-*- coding: euc-kr -*-
 import cv2 
 import mediapipe as mp  
 from PIL import ImageFont, ImageDraw, Image
@@ -39,8 +8,7 @@ import numpy as np
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 mp_drawing_styles = mp.solutions.drawing_styles
-
-# For webcam input:
+is_image_save_s = 0
 cap = cv2.VideoCapture(0)
 
 
@@ -55,20 +23,15 @@ with mp_hands.Hands(
     if not success:
       print("Ignoring empty camera frame.")
 
-      # If loading a video, use 'break' instead of 'continue'.
+
       continue
 
-    # Flip the image horizontally for a later selfie-view display, and convert
-    # the BGR image to RGB.
     image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
 
-    # To improve performance, optionally mark the image as not writeable to
-    # pass by reference.
     image.flags.writeable = False
     results = hands.process(image)
 
 
-    # Draw the hand annotations on the image.
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     image_height, image_width, _ = image.shape
@@ -77,7 +40,7 @@ with mp_hands.Hands(
       for hand_landmarks in results.multi_hand_landmarks:
 
 
-        # 엄지를 제외한 나머지 4개 손가락의 마디 위치 관계를 확인하여 플래그 변수를 설정합니다. 손가락을 일자로 편 상태인지 확인합니다.
+
         thumb_finger_state = 0
         if hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_CMC].y * image_height > hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_MCP].y * image_height:
           if hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_MCP].y * image_height > hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_IP].y * image_height:
@@ -109,12 +72,12 @@ with mp_hands.Hands(
               pinky_finger_state = 1
 
 
-        # 손가락 위치 확인한 값을 사용하여 가위,바위,보 중 하나를 출력 해줍니다.
+
         font = ImageFont.truetype("fonts/gulim.ttc", 80)
         image = Image.fromarray(image)
         draw = ImageDraw.Draw(image)
 
-       
+        
         text = ""
         if thumb_finger_state == 1 and index_finger_state == 1 and middle_finger_state == 1 and ring_finger_state == 1 and pinky_finger_state == 1:
           text = "보"
@@ -126,7 +89,8 @@ with mp_hands.Hands(
           text = "주먹"
         elif   index_finger_state == 0 and middle_finger_state == 1 and ring_finger_state == 0 and pinky_finger_state == 0:
           text = "뻐큐"
-
+        if text == "가위":
+            is_image_save_s = 1
         w, h = font.getsize(text)
 
         x = 50
@@ -137,7 +101,7 @@ with mp_hands.Hands(
         image = np.array(image)
 
 
-        # 손가락 뼈대를 그려줍니다.
+
         mp_drawing.draw_landmarks(
             image,
             hand_landmarks,
@@ -146,7 +110,9 @@ with mp_hands.Hands(
             mp_drawing_styles.get_default_hand_connections_style())
 
     cv2.imshow('MediaPipe Hands', image)
-
+    if is_image_save_s == 1:
+        cv2.imwrite('scissors.jpg', image)
+        is_image_save_s = 0
     if cv2.waitKey(5) & 0xFF == 27:
       break
 
